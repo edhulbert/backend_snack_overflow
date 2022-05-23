46f5c4e2b34f3d71602b777d031031c075385f03
adanabdillahi <adanabdillahi30@gmail.com>
2022-05-23 12:14:32 +0100

    Enum list including regions of foods created

--- src/main/java/com/bnta/backend_snack_overflow/models/Cuisine.java
+++ src/main/java/com/bnta/backend_snack_overflow/models/Cuisine.java
@@ -1,4 +1,15 @@
 package com.bnta.backend_snack_overflow.models;
 
 public enum Cuisine {
+    ITALIAN,
+    TURKISH,
+    IRANIAN,
+    GREEK,
+    SPANISH,
+    NIGERIAN,
+    INDIAN,
+    AMERICAN,
+    MEXICAN,
+    CHINESE,
+    MEDITERRANEAN
 }
