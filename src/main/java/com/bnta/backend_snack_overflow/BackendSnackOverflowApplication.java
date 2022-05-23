70bb19a0f9b32bdd854a8b5d49a2b0f168b65dac
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-05-23 11:25:55 +0100

    first commit

---
+++ src/main/java/com/bnta/backend_snack_overflow/BackendSnackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.bnta.backend_snack_overflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSnackOverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSnackOverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bnta/backend_snack_overflow/BackendSnackOverflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.backend_snack_overflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSnackOverflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
