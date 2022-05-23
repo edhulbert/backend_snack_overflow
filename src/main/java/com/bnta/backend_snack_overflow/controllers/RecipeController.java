8a178493dfcf2229853e532b84ba6084e8159c81
Fiyin Ayodele <fiyinayodele29@gmail.com>
2022-05-23 16:05:59 +0100

    Recipe controller and show route added

--- src/main/java/com/bnta/backend_snack_overflow/controllers/RecipeController.java
+++ src/main/java/com/bnta/backend_snack_overflow/controllers/RecipeController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("recipes")
@@ -22,5 +24,12 @@ public class RecipeController {
     @GetMapping
     public ResponseEntity<List<Recipe>> getAllRecipes() {
         return new ResponseEntity<>(recipeRepository.findAll(), HttpStatus.OK);
+    }
+
+    //SHOW
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Recipe>> getRecipe(@PathVariable Long id) {
+        var recipe = recipeRepository.findById(id);
+        return new ResponseEntity<>(recipe, recipe.isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK);
     }
 }
