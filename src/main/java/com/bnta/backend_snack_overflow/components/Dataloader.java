7c2a96102e562c8e9ab29e3898689b9bf8ecdffd
Ed Hulbert <edward.hulbert@hotmail.co.uk>
2022-05-23 14:35:14 +0100

    added "save to repositories"

--- src/main/java/com/bnta/backend_snack_overflow/components/Dataloader.java
+++ src/main/java/com/bnta/backend_snack_overflow/components/Dataloader.java
@@ -45,7 +45,7 @@ public class Dataloader implements ApplicationRunner {
         Equipment equ11 = new Equipment("Rolling pin");
         Equipment equ12 = new Equipment("Oven");
         Equipment equ13 = new Equipment("Baking tray");
-
+        equipmentRepository.saveAll(Arrays.asList(equ1,equ2,equ3,equ4,equ5,equ6,equ7,equ8,equ9,equ10,equ11,equ12,equ13));
 
         // new ingredients
         Ingredient ingredient1 = new Ingredient("Egg");
@@ -64,6 +64,9 @@ public class Dataloader implements ApplicationRunner {
         Ingredient ingredient14 = new Ingredient("Beans");
         Ingredient ingredient15 = new Ingredient("Chilli powder");
         Ingredient ingredient16 = new Ingredient("Cheese");
+        ingredientRepository.saveAll(Arrays.asList(ingredient1,ingredient2,ingredient3,ingredient4,ingredient5,ingredient6,
+                ingredient7,ingredient8,ingredient9,ingredient10,ingredient11,ingredient12,ingredient13,ingredient14,ingredient15,ingredient16));
+
 
         // new recipes
         Recipe recipe1 = new Recipe("BBQ Chicken Pizza",10, 20, 1, Cuisine.ITALIAN, Arrays.asList(equ12, equ13, equ11, equ9, equ4, equ7));
@@ -73,6 +76,8 @@ public class Dataloader implements ApplicationRunner {
         Recipe recipe5 = new Recipe("Tikka Masala",10, 30, 1, Cuisine.INDIAN, Arrays.asList(equ3, equ4, equ7));
         Recipe recipe6 = new Recipe("Chow Mein",20, 10, 2, Cuisine.CHINESE, Arrays.asList(equ5, equ3, equ7));
         Recipe recipe7 = new Recipe("Nigerian Jollof Rice",20, 30, 4, Cuisine.NIGERIAN, Arrays.asList(equ8, equ1, equ7, equ9));
+        recipeRepository.saveAll(Arrays.asList(recipe1,recipe2,recipe3,recipe4,recipe5,recipe6,recipe7));
+
 
         // new measurements
         Measurement measurement1 = new Measurement(recipe1, ingredient1, "1");
@@ -84,6 +89,7 @@ public class Dataloader implements ApplicationRunner {
         Measurement measurement7 = new Measurement(recipe1, ingredient16, "As much as you need");
         Measurement measurement8 = new Measurement(recipe1, ingredient7, "100g");
 
+
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
@@ -100,5 +106,8 @@ public class Dataloader implements ApplicationRunner {
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
 //        Measurement measurement6 = new Measurement(recipe1, ingredient13, "1");
+
+        measurementRepository.saveAll(Arrays.asList(measurement1,measurement2,measurement3,measurement4,measurement5,measurement6,
+                measurement7,measurement8));
     }
 }
