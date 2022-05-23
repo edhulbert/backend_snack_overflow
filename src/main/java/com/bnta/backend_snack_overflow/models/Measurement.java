75cd3d2b4421659eab62edc06439db14ffc67af1
adanabdillahi <adanabdillahi30@gmail.com>
2022-05-23 12:07:06 +0100

    Measurement model complete

--- src/main/java/com/bnta/backend_snack_overflow/models/Measurement.java
+++ src/main/java/com/bnta/backend_snack_overflow/models/Measurement.java
@@ -1,4 +1,53 @@
 package com.bnta.backend_snack_overflow.models;
 
 public class Measurement {
+    private Long id;
+    private Recipe recipe;
+    private Ingredient ingredient;
+    private String ingredientAmount;
+
+    protected Measurement () {}
+    public Measurement(Recipe recipe, Ingredient ingredient, String ingredientAmount) {
+        this.recipe = recipe;
+        this.ingredient = ingredient;
+        this.ingredientAmount = ingredientAmount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public Ingredient getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(Ingredient ingredient) {
+        this.ingredient = ingredient;
+    }
+
+    public String getIngredientAmount() {
+        return ingredientAmount;
+    }
+
+    public void setIngredientAmount(String ingredientAmount) {
+        this.ingredientAmount = ingredientAmount;
+    }
+
+    @Override
+    public String toString() {
+        return "Measurement{" +
+                "id=" + id +
+                ", recipe=" + recipe +
+                ", ingredient=" + ingredient +
+                ", ingredientAmount='" + ingredientAmount + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/bnta/backend_snack_overflow/models/Recipe.java
+++ src/main/java/com/bnta/backend_snack_overflow/models/Recipe.java
@@ -30,10 +30,6 @@ public class Recipe {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
